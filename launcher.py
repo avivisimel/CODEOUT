49f7245ba2c72869a0c6d7c55a6110d2295ff79b
avivisimel <avivsimelwork@gmail.com>
2022-11-27 13:46:41 +0200

    README.md

--- launcher.py
+++ launcher.py
@@ -2,4 +2,14 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 root = tk.Tk()
-root.title("CODEOUT")
+root.title("CODEOUT launcher")
+
+class Button:
+
+    row = 0
+    column = 0
+    def __init__(self, text, func, arg, image=""):
+        if image!= "":
+            image = tk.PhotoImage(file=image)
+        root.rowconfigure(Button.row, weight=1, minsize=40)
+        root.rowconfigure(Button.column, weight=1, minsize=40)
